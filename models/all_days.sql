1f2542cd3dcae8c94f9d2a0da286710db589344b
Andrew Garschke <AGarschke@underarmour.com>
2021-03-10 19:32:30 +0000

    test

--- models/all_days.sql
+++ models/all_days.sql
@@ -1,6 +1,12 @@
+{{ config (
+    materialized="table"
+)}}
+
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2019-01-01' as date)",
     end_date="cast('2020-01-01' as date)"
    )
 }}
+
+{# instead of "cast('2019-01-01' as date)" we could use "todate('2019-01-01', 'mm/dd/yyyy')" #}
